2c39b3f2b46f1b06bfc3baa0d336120c33866469
Tomoo Kikuchi <48982211+KikuchiTomo@users.noreply.github.com>
2025-12-10 20:49:38 +0900

    fix: improve web uis

--- imq-gui/Resources/Public/js/components/configEditor.js
+++ imq-gui/Resources/Public/js/components/configEditor.js
@@ -13,10 +13,70 @@ function createConfigEditor(configStore) {
 
         async init() {
             await this.store.init();
+
+            // Initialize config with defaults if needed
+            if (!this.config.checks) {
+                this.config.checks = [];
+            }
+            if (!this.config.githubToken) {
+                this.config.githubToken = '';
+            }
+            if (!this.config.githubAPIURL) {
+                this.config.githubAPIURL = 'https://api.github.com';
+            }
+            if (!this.config.githubMode) {
+                this.config.githubMode = 'polling';
+            }
+            if (!this.config.pollingInterval) {
+                this.config.pollingInterval = 60;
+            }
+            if (!this.config.triggerLabel) {
+                this.config.triggerLabel = 'merge-queue';
+            }
+            if (!this.config.databasePath) {
+                this.config.databasePath = '~/.imq/imq.db';
+            }
+            if (!this.config.databasePoolSize) {
+                this.config.databasePoolSize = 5;
+            }
+            if (!this.config.logLevel) {
+                this.config.logLevel = 'info';
+            }
+            if (!this.config.logFormat) {
+                this.config.logFormat = 'pretty';
+            }
+        },
+
+        addCheck() {
+            if (!this.config.checks) {
+                this.config.checks = [];
+            }
+            this.config.checks.push({
+                name: '',
+                type: 'githubActions',
+                workflowName: '',
+                jobName: '',
+                scriptPath: '',
+                arguments: '',
+                timeout: null,
+                failFast: 'true'
+            });
+        },
+
+        removeCheck(index) {
+            if (this.config.checks && this.config.checks.length > index) {
+                this.config.checks.splice(index, 1);
+            }
         },
 
         async save() {
             await this.store.save();
+        },
+
+        reset() {
+            if (confirm('Are you sure you want to reset all settings to defaults?')) {
+                this.store.reset();
+            }
         }
     };
 }
